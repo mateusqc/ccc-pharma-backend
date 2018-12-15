ab6aa7040843acc265883fef27f8f24b31534c5e
Thomaz Diniz <thomaz_diniz@hotmail.com>
2018-12-15 02:32:19 -0300

    refactor token

--- backend/src/main/java/com/cccpharma/app/auth/TokenAuthenticationService.java
+++ backend/src/main/java/com/cccpharma/app/auth/TokenAuthenticationService.java
@@ -13,7 +13,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 public class TokenAuthenticationService {
-	static final long EXPIRATION_TIME = 860_000_000;
+	static final long EXPIRATION_TIME = 900_000_000;
 	static final String SECRET = "Secret";
 	static final String TOKEN_PREFIX = "Bearer";
 	static final String HEADER_STRING = "Authorization";
