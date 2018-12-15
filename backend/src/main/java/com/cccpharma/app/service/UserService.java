0d9624d87ec140e2cab118cb2db7fb851efe67a1
mateusqc <mateusqcunha@gmail.com>
2018-12-15 00:37:19 -0300

    user rest added

--- backend/src/main/java/com/cccpharma/app/service/UserService.java
+++ backend/src/main/java/com/cccpharma/app/service/UserService.java
@@ -2,10 +2,12 @@ package com.cccpharma.app.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cccpharma.app.model.Product;
 import com.cccpharma.app.model.User;
 import com.cccpharma.app.repository.UserRepository;
 
@@ -26,7 +28,37 @@ public class UserService {
 		return userRepository.findByEmail(email);
 	}
 	
+	public User getUserById(Long id) {
+		Optional<User> userData = userRepository.findById(id);
+		return userData.isPresent()? userData.get() : null;
+	}
+	
 	public User createUser(User user) {
 		return userRepository.save(user);
+	}
+	
+	public User updateUser(Long id, User user) {
+		Optional<User> userData = userRepository.findById(id);
+		if (userData.isPresent()) {
+			User savedUser = userData.get();
+			savedUser.setName(user.getName());
+			savedUser.setPassword(user.getPassword());
+			savedUser.setRole(user.getRole());
+			if (!savedUser.getEmail().equals(user.getEmail()) || savedUser.getId() != user.getId()) {
+				return null;
+			}
+			return userRepository.save(savedUser);
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean deleteUser(Long id) {
+		try {
+			userRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
