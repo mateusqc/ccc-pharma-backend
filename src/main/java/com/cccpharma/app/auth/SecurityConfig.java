0c270b68a3bcdd5c20bb79d9e39d3b9cddcf1a1c
Thomaz Diniz <thomaz_diniz@hotmail.com>
2018-12-15 04:56:39 -0300

    cors config

--- src/main/java/com/cccpharma/app/auth/SecurityConfig.java
+++ src/main/java/com/cccpharma/app/auth/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.cccpharma.app.auth;
 
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
  
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableAutoConfiguration
@@ -29,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().authorizeRequests()
+		http.cors().and().csrf().disable().authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/login").permitAll()
 //			.anyRequest().authenticated()
 			//.and().authorizeRequests().antMatchers("/").hasRole("ADMIN")
@@ -60,5 +65,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public static NoOpPasswordEncoder passwordEncoder() {
 		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
+	
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
 
 }
