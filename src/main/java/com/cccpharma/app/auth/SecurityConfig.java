57cf4892d29d2e62818bbeb5b1ee02f534209ab4
Thomaz Diniz <thomaz_diniz@hotmail.com>
2018-12-16 17:29:18 -0300

    Update SecurityConfig.java
    Add Expose Headers

--- src/main/java/com/cccpharma/app/auth/SecurityConfig.java
+++ src/main/java/com/cccpharma/app/auth/SecurityConfig.java
@@ -74,8 +74,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-type", "x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
